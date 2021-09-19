9b60e1021225b24716d3645ba351a5cea6f40250
Lenny <kiddoyk@gmail.com>
2021-09-19 16:21:51 +0800

    updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar>
+          <Typography.title level={2} className="logo">
+            <Link to="/">Cryptoverse</Link>
+          </Typography.title>
+          {/* <Button className='menu-cnotrol-container'></Button> */}
+        </Avatar>
+      </div>
+    </div>
+  );
+};
