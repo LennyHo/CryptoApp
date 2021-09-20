6c327d5a1fbc8c8c194174701c549b7ca4101663
Lenny <kiddoyk@gmail.com>
2021-09-20 16:03:33 +0800

    updated

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,31 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
+// import Loader from './Loader';
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
 // captialize the variables with big caps
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
-  const [cryptos] = useState(cryptosList?.data?.coins);
+const Cryptocurrencies = (simplified) => {
+  // if statement
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+
+    const filteredData = cryptosList?.data?.coins.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return "loading..";
 
   return (
     <>
-      <Row gutters={(32, 32)} className="crypto-card-container">
-        {cryptos.map((currency) => {
-          <Col xs={24} sm={12} ig={6} className="crypto-card">
+      {/* {!simplified && ( */}
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+          />
+        </div>
+      {/* )} */}
+
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card">
             <Link key={currency.id} to={`/crypto/${currency.id}`}>
               <Card
                 title={`${currency.rank}.${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />} hoverable>
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                hoverable
+              >
                 <p>Price:{millify(currency.price)}</p>
                 <p>Market Cap:{millify(currency.marketCap)}</p>
                 <p>Dailt Change:{millify(currency.change)}%</p>
               </Card>
             </Link>
-        </Col>;
-        })}
+          </Col>
+        ))}
+        ;
       </Row>
     </>
   );
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
-  return <div>news</div>;
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: 'Cryptocurrency',
+    count: simplified ? 10 : 100,
+  });
+
+  console.log(cryptoNews);
+
+  return <div>News</div>;
 };
 
 export default News;
